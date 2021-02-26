4bd0173a8e20c7217cff71ed8bbe5bcb884907d6
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-02-26 16:55:05 -0300

    rest dados

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const materias = [];
 
-let materias = [];
+router.use(express.urlencoded({ 
+    extended: true 
+}));
 
 router.get('/', (req, res) => {
 	res.render('index');		
 });
   
+router.post('/submit-form', (req, res) => {
+    const materia = {
+        nome: req.body.materia,
+        aulasSemanais: req.body.aulasSemanais,
+        disponibilidadeSegunda: req.body.disponibilidadeSegunda
+    };
+
+    materias.push(materia);
+    console.log(materias);
+})
 
 module.exports = router;
