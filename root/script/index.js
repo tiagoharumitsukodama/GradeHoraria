78f3eec4239b0eafd14182c5ab8cb760d432a155
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-03-12 15:58:09 -0300

    terminei o front

--- root/script/index.js
+++ root/script/index.js
@@ -1,30 +1,47 @@
-const express = require('express');
-const router = express.Router();
-const cadastrar = require('./cadastrar');
-const organizar = require('./organizar');
-const montarTabela = require('./tabelarMatriz')
+function addTurma() {
 
-const TodasTurmas = {};
+    function ativarTabela(){
+        let tabela = document.querySelector("table#inserir_dados");
+        let bt = document.querySelector("#IdaddComponente");
+        let div = document.querySelector("div.inserir_dados_iniciais");
 
-router.use(express.urlencoded({ 
-    extended: true 
-}));
+        tabela.style.display = "block";
+        bt.style.display = "block";
+        div.style.display = "none";
+    }
 
-router.get('/', (req, res) => {
-	res.render('index');		
-});
-  
-router.post('/submit-form', (req, res) => {
+    ativarTabela();
 
-    cadastrar.alimetar(req.body, TodasTurmas);
+    let listaTurma = document.querySelector(".inserir_dados_iniciais input").value.split(',');
+    let listatr = document.querySelectorAll("table#inserir_dados tr");
+    let titulo = listatr[0]
+    let campo = listatr[1];
 
-    let listaMatriz = organizar.listaMatrizEpontuacao(TodasTurmas);
-    let elementoTabela = montarTabela.montarTabela(listaMatriz[0].matriz);
+    for ( keyTurma of listaTurma ) {
 
-    console.log(elementoTabela);
-    //Devolver as opções
-    //res.send(elementoTabela);
-    res.end();
-})
+        if (keyTurma !== "") {
 
-module.exports = router;
+            let th = document.createElement('TH');
+            th.innerHTML = 
+                `<td>
+                ${keyTurma}
+                </td>`;
+            titulo.appendChild(th);
+    
+            let td = document.createElement('TD');
+            td.innerHTML = 
+                `<td>
+                <input type="number" name="numeroAulas${keyTurma}" value="0" style="width: 50px; text-align: center;" >
+                </td>`;
+            campo.appendChild(td);
+        }
+    }
+}
+
+function addComponente() {
+    let tabela = document.querySelector("#inserir_dados");
+    let linha = document.querySelectorAll("#inserir_dados tr")[1];
+
+    tabela.innerHTML += linha.innerHTML;
+}
+
