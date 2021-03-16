64b294e4cb49251dee0496d6e002e3ff3ba6aeb6
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-03-16 03:36:40 -0300

    mostrar tabela

--- root/script/index.js
+++ root/script/index.js
@@ -1,7 +1,9 @@
 import {addTurma, addComponente, extrairDados} from './inserirDados.js';
 import listaMatrizEpontuacao from './organizar.js';
+import montarTabela from './tabelarMatriz.js';
 
 let TodasTurmas = {};
+let listaMatriz;
 
 
 let buttonTurma = document.querySelector('#IdaddTurma');
@@ -10,10 +12,23 @@ buttonTurma.addEventListener('click', addTurma);
 
 let btEnviar = document.querySelector("#Idenviar");
 btEnviar.addEventListener('click', () => {
-    extrairDados(TodasTurmas)
 
-    let listaMatriz = listaMatrizEpontuacao(TodasTurmas);
+    extrairDados(TodasTurmas);
+    listaMatriz = listaMatrizEpontuacao(TodasTurmas);    
 
-    
 });
+
+
+let counterListaMatriz = 0;
+let botaoResultado = document.querySelector("#botaoResultado");
+botaoResultado.addEventListener('click', () => {
+
+    let tabela = montarTabela (listaMatriz[counterListaMatriz++].matriz);
+
+
+    let divResultado = document.querySelector("#resultado");
+    divResultado.innerHTML = tabela;
+})
+
+
 
